c539ebded859e9de51ef21c5b648a77a9626e0af
Ajith Kumar S <ajithkumars1609@gmail.com>
2024-05-29 16:39:22 +0530

    star rating count and star rating message project added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const message = [
+    "TERRIBLE",
+    "BAD",
+    "OK",
+    "GOOD",
+    "AMAZING"
+]
+
+export default function App(){
+    return (
+        <div className='app'>
+            <StartRating />
+            <StartRating message={message}/>
+        </div>
+    )
 }
 
-export default App;
+function StartRating({message = []}){
+    
+    const [rating,setRating] = useState(0);
+    const [tempRating,setTempRating] = useState(0);
+
+    function handleStarRating(rating){
+        setRating(rating);
+    }
+
+    return (
+        <div className="star-container">
+            {
+                Array.from({length:5},(_,i) => <Star onRating={() => handleStarRating(i + 1)} full={tempRating ? tempRating >= i + 1 :rating >= i + 1} onHoverIn={() => setTempRating(i + 1)} onHoverOut={() => setTempRating(0)} key={i}/>)
+            }
+            <Count rating={rating} tempRating={tempRating} message={message}/>
+        </div>
+    )
+}
+
+function Star({onRating,full,onHoverIn,onHoverOut}){
+    return (
+        <span className="star" role='button' onClick={onRating} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
+            {
+                full ? <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="#ffa534"
+                stroke="#ffa534"
+              >
+                <path
+                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                />
+              </svg>
+              :
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="#ffa534"
+            >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="{2}"
+                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                />
+            </svg>
+            }
+        </span>
+    )
+}
+
+function Count({rating,tempRating,message}){
+    return (
+        <p className='star-count'>{message.length > 0 ? message[tempRating ? tempRating - 1 :rating-1] :tempRating || rating || ''}</p>
+    )
+}
+
+
+
